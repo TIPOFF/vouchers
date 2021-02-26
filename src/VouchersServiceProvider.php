590e1a4f0643f688ade5f6bf795ba3ab45eb6086
Patrick Breen <patrick.breen@quartzy.com>
2021-02-26 13:38:18 -0500

    Remove old listener and tests

--- src/VouchersServiceProvider.php
+++ src/VouchersServiceProvider.php
@@ -4,12 +4,10 @@ declare(strict_types=1);
 
 namespace Tipoff\Vouchers;
 
-use Tipoff\Checkout\Events\BookingOrderProcessed;
 use Tipoff\Support\Contracts\Checkout\Vouchers\VoucherInterface;
 use Tipoff\Support\TipoffPackage;
 use Tipoff\Support\TipoffServiceProvider;
 use Tipoff\Vouchers\Listeners\OrderCreatedListener;
-use Tipoff\Vouchers\Listeners\PartialRedemptionCheck;
 use Tipoff\Vouchers\Models\Voucher;
 use Tipoff\Vouchers\Models\VoucherType;
 use Tipoff\Vouchers\Policies\VoucherPolicy;
@@ -38,9 +36,6 @@ class VouchersServiceProvider extends TipoffServiceProvider
                 VoucherInterface::class => Voucher::class,
             ])
             ->hasEvents([
-                BookingOrderProcessed::class => [
-                    PartialRedemptionCheck::class,
-                ],
                 OrderCreatedListener::class => [
                     OrderCreatedListener::class,
                 ],

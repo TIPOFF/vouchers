d290935aa64654f62ec67ede3f73616ac6cc8c3e
Wolfred Montilla <wolfrednicolas@gmail.com>
2021-03-25 20:44:55 -0400

    add nova test

--- tests/Feature/Nova/VoucherTypeResourceTest.php
+++ tests/Feature/Nova/VoucherTypeResourceTest.php
@@ -7,21 +7,118 @@ namespace Tipoff\Vouchers\Tests\Feature\Nova;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tipoff\Vouchers\Models\VoucherType;
 use Tipoff\Vouchers\Tests\TestCase;
+use Tipoff\Authorization\Models\User;
 
 class VoucherTypeResourceTest extends TestCase
 {
     use DatabaseTransactions;
 
-    /** @test */
-    public function index()
+    private const NOVA_ROUTE = 'nova-api/voucher-types';
+
+    /**
+     * @dataProvider dataProviderForIndexByRole
+     * @test
+     */
+    public function index_by_role(?string $role, bool $hasAccess, bool $canIndex)
     {
+        $user = User::factory()->create();
+        if ($role) {
+            $user->assignRole($role);
+        }
+
+        $this->actingAs($user);
+
         VoucherType::factory()->count(4)->create();
 
-        $this->actingAs(self::createPermissionedUser('view voucher types', true));
+        $response = $this->getJson(self::NOVA_ROUTE)
+            ->assertStatus($hasAccess ? 200 : 403);
 
-        $response = $this->getJson('nova-api/voucher-types')
-            ->assertOk();
+        if ($hasAccess) {
+            $this->assertCount($canIndex ? 4 : 0, $response->json('resources'));
+        }
+    }
 
-        $this->assertCount(4, $response->json('resources'));
+    public function dataProviderForIndexByRole()
+    {
+        return [
+            'Admin' => ['Admin', true, true],
+            'Owner' => ['Owner', true, true],
+            'Executive' => ['Executive', true, true],
+            'Staff' => ['Staff', true, true],
+            'Former Staff' => ['Former Staff', false, false],
+            'Customer' => ['Customer', false, false],
+            'No Role' => [null, false, false],
+        ];
+    }
+
+    /**
+     * @dataProvider dataProviderForShowByRole
+     * @test
+     */
+    public function show_by_role(?string $role, bool $hasAccess, bool $canView)
+    {        
+
+        $user = User::factory()->create();
+        if ($role) {
+            $user->assignRole($role);
+        }
+        $this->actingAs($user);
+
+        $model = VoucherType::factory()->create();
+
+        $response = $this->getJson(self::NOVA_ROUTE . "/{$model->id}")
+            ->assertStatus($hasAccess ? 200 : 403);
+
+        if ($hasAccess && $canView) {
+            $this->assertEquals($model->id, $response->json('resource.id.value'));
+        }
+    }
+
+    public function dataProviderForShowByRole()
+    {
+        return [
+            'Admin' => ['Admin', true, true],
+            'Owner' => ['Owner', true, true],
+            'Executive' => ['Executive', true, true],
+            'Staff' => ['Staff', true, true],
+            'Former Staff' => ['Former Staff', false, false],
+            'Customer' => ['Customer', false, false],
+            'No Role' => [null, false, false],
+        ];
+    }
+
+    /**
+     * @dataProvider dataProviderForDeleteByRole
+     * @test
+     */
+    public function delete_by_role(?string $role, bool $hasAccess, bool $canDelete)
+    {
+        $user = User::factory()->create();
+        if ($role) {
+            $user->assignRole($role);
+        }
+        $this->actingAs($user);
+
+        $model = VoucherType::factory()->create();
+
+        // Request never fails
+        $this->deleteJson(self::NOVA_ROUTE . "?resources[]={$model->id}")
+            ->assertStatus($hasAccess ? 200 : 403);
+
+        // But deletion will only occur if user has permissions
+        $this->assertDatabaseCount('voucher_types', $canDelete ? 0 : 1);
+    }
+
+    public function dataProviderForDeleteByRole()
+    {
+        return [
+            'Admin' => ['Admin', true, false],
+            'Owner' => ['Owner', true, false],
+            'Executive' => ['Executive', true, false],
+            'Staff' => ['Staff', true, false],
+            'Former Staff' => ['Former Staff', false, false],
+            'Customer' => ['Customer', false, false],
+            'No Role' => [null, false, false],
+        ];
     }
 }
